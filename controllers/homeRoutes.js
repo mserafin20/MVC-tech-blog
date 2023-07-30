5789f16f24bb108535d5e847853eed87e78c18b7
mserafin20 <mmserafin20@gmail.com>
2023-07-29 21:02:23 -0400

    Added more code to homepageRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,6 +27,54 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get('/post:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.session.user_id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        const posts = postData.get({ plain: true });
 
+        res.render('post', {
+            ...post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Post }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/profile');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 module.exports = router; 
