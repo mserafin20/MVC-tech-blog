3d793251922d2a30b18345bcd1b5670ac5250810
mserafin20 <mmserafin20@gmail.com>
2023-08-01 12:30:50 -0400

    added in seed.js file to seeds folder

---
+++ seeds/seed.js
@@ -0,0 +1,32 @@
+const sequelize = require('../config/connection');
+const { Blogger, Post, Comment } = require('../models');
+
+const bloggerData = require('./bloggerData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await Blogger.bulkCreate(bloggerData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Post.bulkCreate(postData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Comment.bulkCreate(commentData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+
+
+  process.exit(0);
+};
+
+seedDatabase();
